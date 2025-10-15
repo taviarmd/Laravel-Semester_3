635e51c3995d4b2174b13c88ea29e510251014d0
taviarmd <taviarmd@gmail.com>
2025-10-15 11:59:36 +0700

    Penambahan Master Layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -4,6 +4,9 @@
      
 </head> 
 <body> 
+@extends('master') 
+@section('title', 'Daftar Pegawai') 
+@section('content')
     <div class="container mt-5"> 
         <h1 class="mb-4">Daftar Pegawai</h1> 
     <table border="1" cellpadding="5" cellspacing="0"> 
@@ -43,5 +46,6 @@
         </tbody> 
     </table> 
     </div> 
+    @endsection 
 </body> 
 </html> 
